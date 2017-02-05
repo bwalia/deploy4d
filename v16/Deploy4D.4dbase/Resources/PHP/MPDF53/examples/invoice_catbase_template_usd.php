68afa7132c31cbf8c371f085c475518b3aaa59e9
Balinder WALIA <balinder.walia@gmail.com>
2017-02-05 06:51:40 +0000

    v16 deploy

---
+++ v16/Deploy4D.4dbase/Resources/PHP/MPDF53/examples/invoice_catbase_template_usd.php
@@ -0,0 +1,244 @@
+<?php
+$watermarkTxt='OVERDUE';
+
+$html = '
+<style>
+body {font-family: arial;
+    font-size: 8pt;
+}
+div {    margin: 0pt;
+padding: 0pt;
+}
+
+h4 {
+	font-family: sans;
+	font-weight: bold;
+	margin-top: 0.7em;
+	margin-bottom: 0.5em;
+}
+div {
+	padding:1em; 
+	margin-bottom: 1em;
+	text-align:justify; 
+}
+
+.tenthmatrixlogo { position: absolute; 
+	overflow: visible; 
+	left: 22; 
+	top: 0; 
+	padding-bottom: 10px; 
+	font-family:sans; 
+	margin: 0;
+}
+
+.trans_type { position: absolute; 
+color: #666666;
+	overflow: visible; 
+	left: 22; 
+	top: 60; 
+	padding-bottom: 10px; 
+	font-family:sans; 
+	font-size: 26px;
+	margin: 0;
+}
+
+
+td { vertical-align: top; }
+.items td {
+    border-left: 0.1mm solid #000000;
+    border-right: 0.1mm solid #000000;
+}
+
+table thead td { background-color: #EEEEEE;
+    text-align: center;
+    border: 0.1mm solid #000000;
+}
+.items td.blanktotal {
+    background-color: #FFFFFF;
+    border: 0mm none #000000;
+    border-top: 0.1mm solid #000000;
+    border-right: 0.1mm solid #000000;
+}
+.items td.totals {
+    text-align: right;
+    
+    border: 0.1mm solid #333333;
+}
+
+.bankDetails
+{
+	background-color: #FFF;
+	border-collapse: collapse; 
+	border: 1px solid #827F85;
+	margin: 4pt 0pt 0pt 0pt;
+}
+.bankDetails td 
+{
+     border: 1px solid #827F85;
+}
+.bankDetails thead td 
+{ 
+	background-color: #00A3D3;
+	text-align: center;
+	color:#FFF;
+	font-size: 12pt;
+	text-align: center;
+	font-family:Verdana, Geneva, sans-serif;
+}
+
+
+</style>
+
+<body>
+
+<div class="tenthmatrixlogo">
+<img src="catbase-small-transp.png" width="200" />
+</div>
+
+<div class="trans_type">INVOICE</div>
+
+<div style="width: 225px;
+float: right;
+position: absolute; 
+	right: 25; 
+	top: 1; 
+	padding: 8px; 
+	font-family:sans; 
+	font-size: 0.9em;
+	margin: 0;
+">
+
+<img src="tenthmatrix-logo.png" width="190" /><br><h4>Tenthmatrix Information Systems Ltd.</h4>The Coach House, 77A Marlowes, Hemel Hempstead<br />Herts, HP1 1LF<br />Tel: 0207 118 7889<br />Email: billing@tenthmatrix.co.uk</div>
+
+<htmlpagefooter name="myfooter">
+<div style="border-top: 1px solid #333333; font-size: 9pt; text-align: center; padding-top: 1mm; ">
+Page {PAGENO} of {nb}
+</div>
+</htmlpagefooter>
+
+<sethtmlpageheader name="myheader" value="on" show-this-page="1" />
+<sethtmlpagefooter name="myfooter" value="on" />
+
+<div style="margin-top: 90; margin-bottom: 0pt; width: 40%;
+	border:0.1mm solid #220044; 
+	background-color: #f0f2ff;
+	background-gradient: linear #eeeeee #f0f2f0 0 1 0 0.5;">
+
+<h4><b>Bill To:</b></h4>
+Dun & Bradstreet<br />Attn: Accounts Payable<br />6 ARMSTRONG ROAD<br />Shelton,, CT<br /><br />
+</div>
+
+
+<div style="position: absolute; 
+	overflow: visible; 
+	right: 30; 
+	top: 120; 
+	padding-bottom: 10px; 
+	font-family:sans; width: 300px;">
+
+<table width="100%" style="border-collapse: collapse; font-family: sans; border: 1px solid #333333;" cellpadding="8">
+<tr>
+<td style="background-color: #EEEEEE; border-right: 1px solid #333333;border-bottom: 1px solid #333333;"><b>Invoice#</b></td>
+<td style="text-align: center;border-bottom: 1px solid #333333;">3914-001034</td>
+</tr>
+<tr>
+<td style="background-color: #EEEEEE; border-right: 1px solid #333333;border-bottom: 1px solid #333333;"><b>Invoice Date</b></td>
+<td style="text-align: center;border-bottom: 1px solid #333333;">01/10/2015</td>
+</tr>
+<tr>
+<td style="background-color: #EEEEEE; border-right: 1px solid #333333;border-bottom: 1px solid #333333;"><b>Due Date</b></td>
+<td style="text-align: center;border-bottom: 1px solid #333333;">01/10/2015</td>
+</tr>
+<tr>
+<td style="background-color: #EEEEEE; border-right: 1px solid #333333;border-bottom: 1px solid #333333;"><b>Project</b></td>
+<td style="text-align: center;border-bottom: 1px solid #333333;">CATBASE</td>
+</tr>
+<tr>
+<td style="background-color: #EEEEEE; border-right: 1px solid #333333;border-bottom: 1px solid #333333;"><b>Ordered by</b></td>
+<td style="text-align: center;border-bottom: 1px solid #333333;">Carol Vass</td>
+</tr>
+
+<tr>
+<td style="background-color: #EEEEEE; border-right: 1px solid #333333;border-bottom: 1px solid #333333;"><b>Account number</b></td>
+<td style="text-align: center;">3914</td>
+</tr>
+
+</table>
+</div>
+
+<table class="items" width="100%" style="font-size: 7pt; border-collapse: collapse;margin-top: 90px;" cellpadding="8">
+<thead>
+<tr>
+<td width="10%"><b>ITEM</td>
+<td width="10%"><b>QUANTITY</b></td>
+<td width="50%"><b>DESCRIPTION</b></td>
+<td width="15%"><b>UNIT PRICE</b></td>
+<td width="15%"><b>AMOUNT</b></td>
+</tr>
+</thead>
+<tbody>
+
+<!-- ITEMS HERE -->
+<tr>
+
+<td align="center">1</td>
+<td align="center">1</td>
+<td>Product: Technical Support, Single-User<br \/>Technical Support for 1 year</td>
+<td align="right">&#163;1200.00</td>
+<td align="right">&#36;1200.00</td>
+</tr>
+
+<!-- END ITEMS HERE -->
+
+<tr>
+<td class="blanktotal" colspan="3" rowspan="6"></td>
+<td class="totals">Subtotal:</td>
+<td class="totals">&#36;1200.00</td>
+</tr>
+<tr>
+<td class="totals">VAT Rate:</td>
+<td class="totals">&mdash;</td>
+</tr>
+&lt;!--4DIF ([Invoices]inv_shipping>0)--&gt; : 4DENDIF expected
+<tr>
+<tr>
+<td class="totals">VAT:</td>
+<td class="totals">&mdash;</td>
+</tr>
+
+<tr>
+<td class="totals"><b>TOTAL:</b></td>
+<td class="totals"><b>&#36;1200.00</b></td>
+</tr>
+
+<tr>
+<td class="totals"><b>Balance due:</b></td>
+<td class="totals"><b>&#36;1200.00</b></td>
+</tr>
+</tbody>
+</table>
+
+';
+
+$termsHTML = '<h3>Thank you for your business.</h3>
+
+<h5>VAT number: GB 972985556</h5>
+<h5><u>Terms of business</u></h5><p><a href="http://www.tenthmatrix.co.uk/terms-of-business.html">http://www.tenthmatrix.co.uk/terms-of-business.html</a></p>
+
+<p>Please note that our payment terms for this Invoice are OVERDUE days from the date of Invoice. Interest will be charged on all Invoice that are not paid on time.</p>
+<h4><u>Notes</u></h4><p>Technical support, all CatBase program updates, and one full day of custom training, scripting, and help with InDesign.<br /> For period: 01/10/2015 to 01/10/2016</p>
+
+<h4><u>PAYING BY DEBIT OR CREDIT CARD</u></h4><h4>Click on this link <a href="http://localhost:8080/index.shtml?pay=8ce2a5702338f6f254ed4e925dfe2ba0">http://localhost:8080/index.shtml?pay=8ce2a5702338f6f254ed4e925dfe2ba0</a> to Pay this Invoice now.</h4><h4>Invoice PIN: 6131</h4><h5><u>Refunds Policy</u></h5><p><a href="http://www.tenthmatrix.co.uk/terms-of-business.html#refund_policy">http://www.tenthmatrix.co.uk/terms-of-business.html#refund_policy</a></p>
+
+<h4><u>Paying online</u></h4><h4>Please make payment by electronic transfer to:</h4><table class="bankDetails" cellpadding="8" cellspacing="0"  style="width: 50%"><tbody><tr><td>BACS Bank details</td><td>HSBC ( USD Account )</td></tr><tr><td>Account name</td><td>Tenthmatrix</td></tr><tr><td>Sort code</td><td>40-05-15</td></tr><tr><td>Account number</td><td>73160199</td></tr><tr><td>Please use reference</td><td>CATBASE-1034</td></tr></tbody></table><h4>For International bank transfers:</h4><table class="bankDetails" cellpadding="8" cellspacing="0"  style="width: 50%"><tbody><tr><td>IBAN</td><td>GB86MIDL40051573160199</td></tr><tr><td>HSBC BIC code / SWIFT Code</td><td>MIDLGB22</td></tr></tbody></table>
+
+<h4>To make payment via international banking, you simply need to contact your bank with the above bank details. They can advise you if you should use the BIC code or SWIFT Code depending on the payment originating country.</h4>
+
+<p>Tenthmatrix Information Systems Ltd. also trading as Tenthmatrix, is a registered company number: 06597469. VAT number: GB 972 9855 56. Registered in England and Wales. Registered office address: The Coach House, 77A The Marlowes, Hemel Hempstead, Herts, HP1 1LF, United Kingdom. Correspondence office address: 16 The Foxgloves, Hemel Hempstead, Hertfordshire, HP1 2DB, United Kingdom.</p><p>Note: This Invoice was generated by Tenthmatrix CRM and Invoicing system automatically.</p><p>Date: 25/11/2015, Time: 14:32:56</p>
+
+<p align="right">E&OE</p>
+</body>';
+
+//==============================================================//==============================================================//==============================================================include("/Users/balinderwalia/Dropbox/4D Databases/4Dv14_Dev/TenthmatrixCRMv14/TENTHMATRIX_CRM_DATA/php_components/MPDF53/mpdf.php");$mpdf=new mPDF('utf-8','A4','','',15,14,10,20,10,10); $mpdf->SetWatermarkText($watermarkTxt,0.05);$mpdf->watermark_font = 'DejaVuSansCondensed';$mpdf->showWatermarkText = true;$mpdf->SetDisplayMode('fullpage');$mpdf->WriteHTML($html . $termsHTML);	// Separate Paragraphs  defined by font$mpdf->Output('/Users/balinderwalia/Dropbox/4D Databases/4Dv14_Dev/TenthmatrixCRMv14/Invoices/Invoice-3914-1034.pdf','F');exit;//==============================================================//==============================================================//==============================================================
+
+?>
---
+++ v16/Deploy4D.4dbase/Resources/PHP/MPDF53/examples/order_website_template_gbp_html.php
@@ -0,0 +1,17 @@
+Dear &lt;!--#4DVAR s80EmailName--&gt; : Undefined
+<br><br>
+Thank you for your recent enquiry about WEBSITE.
+<br><br>
+Please find the attached Order.
+<br><br>
+Attached Order# 1275-000888
+<br><br>
+Terms of business
+<br><br>
+http://www.tenthmatrix.co.uk/terms-of-business.html
+<br><br>Notes
+<br><br>
+Control Panel changes as per my discussion with Marc.
+<br><br>
+<br><br>Balinder Walia<br>-<br>Best regards<br>Tenthmatrix Sales<br>e: sales@tenthmatrix.co.uk<br>w: www.tenthmatrix.co.uk<br>p: 44 (0)207 118 7889<br>
+t: @tenthmatrix<br><br>Find out more about Tenthmatrix Products today:<br><br>1) CatBase: The Database Publishing Solution W > www.catbase.com<br>2) Jobshout: Empowering the job search technology W > www.jobshout.co.uk<br><br>Note: This email was sent by Tenthmatrix CRM and Invoicing system automatically. E&OE<br><br>Tenthmatrix is a specialist IT software development and Integrated web marketing company<br>Tenthmatrix is trying to reduce its carbon footprint so we ask that you don't print this e-mail unless it is absolutely necessary. Thank you.<br>__________________________________________________________________________<br>Tenthmatrix Information Systems Ltd t/a Tenthmatrix. Registered company number: 6597469 Registered in England & Wales<br><br>This email and attachments are confidential. Any review, retransmission,dissemination or reliance upon them, or use of this information by otherthan the intended recipient is prohibited. If you have received this messagein error please contact the sender and delete it. Thank you.
